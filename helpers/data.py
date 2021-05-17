80d755bbd2d04507e13dec26977e60f347a0cf9b
nzupancic <17457088+clearedTakeoff@users.noreply.github.com>
2021-05-17 19:52:50 +0200

    Merge custom features branch to master

--- helpers/data.py
+++ helpers/data.py
@@ -6,26 +6,31 @@ from helpers.preprocess import preprocess_data
 from helpers.custom_features import get_custom_features
 
 
-def get_data():
+def get_data(include_stories=False):
     le = preprocessing.LabelEncoder()
     le.fit(ALL_CLASSES)  # Encode classes with numerical labels
     X, y = read_worksheet(DATASET_PATH, "Discussion only data", ALL_CLASSES, le, 10)
     X2, y2 = read_worksheet(DATASET_PATH, "CREW data", ALL_CLASSES, le, 11)
     X.extend(X2)  # Join worksheets into a single dataset
     y.extend(y2)
-
-    # include stories as content discussion
-    X_s, y_s = read_stories(STORIES_PATH, le)
-    X.extend(X_s)
-    y.extend(y_s)
+    if include_stories:
+        # include stories as content discussion
+        X_s, y_s = read_stories(STORIES_PATH, le)
+        X.extend(X_s)
+        y.extend(y_s)
     return preprocess_data(X, y)
 
 
-def get_data_with_features():
+def get_data_with_features(include_stories=False):
     le = preprocessing.LabelEncoder()
     le.fit(ALL_CLASSES)  # Encode classes with numerical labels
     X, y = read_worksheet(DATASET_PATH, "Discussion only data", ALL_CLASSES, le, 10)
     X2, y2 = read_worksheet(DATASET_PATH, "CREW data", ALL_CLASSES, le, 11)
     X.extend(X2)  # Join worksheets into a single dataset
     y.extend(y2)
+    if include_stories:
+        # include stories as content discussion
+        X_s, y_s = read_stories(STORIES_PATH, le)
+        X.extend(X_s)
+        y.extend(y_s)
     return get_custom_features(X, y)
--- naive_bayes.py
+++ naive_bayes.py
@@ -1,9 +1,6 @@
-import os
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import make_pipeline, FeatureUnion
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import KFold, cross_validate
 from helpers.scorers import scorers_dict, generate_report
 from helpers.data import get_data, get_data_with_features
@@ -21,13 +18,21 @@ class extractColumn(object):
 
 
 if __name__ == "__main__":
-    X, y = get_data_with_features()
+    # TF-IDF only
+    X, y = get_data(include_stories=False)
+
+    # Naive Bayes classifier pipeline
+    model = make_pipeline(TfidfVectorizer(sublinear_tf=True, use_idf=True, ngram_range=(1, 2)),
+                          MultinomialNB(alpha=0.01))
+
+    # Use TF-IDF together with custom features
+    """X, y = get_data_with_features(include_stories=False)
 
     tfidf_pipe = make_pipeline(extractColumn(0), TfidfVectorizer(sublinear_tf=True, use_idf=True))
     custom_pipe = make_pipeline(extractColumn(1))
 
     # Naive Bayes classifier pipeline
-    model = make_pipeline(FeatureUnion([("tfidf", tfidf_pipe), ("custom_features", custom_pipe)]), MultinomialNB(alpha=0.01))
+    model = make_pipeline(FeatureUnion([("tfidf", tfidf_pipe), ("custom_features", custom_pipe)]), MultinomialNB(alpha=0.01))"""
 
     # Evaluate classifier using KFold cross validation
     kf = KFold(n_splits=5, shuffle=True, random_state=77)
