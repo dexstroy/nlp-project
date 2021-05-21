2c81462b7a44e45948a3043dacc05bd552e3faf9
Aleks Vujic <aleks.vujic@gmail.com>
2021-05-21 13:05:06 +0200

    Increased number of iterations for logistic regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -7,6 +7,8 @@ from helpers.data import get_data, get_data_with_features, ExtractColumn
 
 
 def logistic_regression_classifier(use_stories=False, use_custom_features=False, use_class_grouping=False):
+    lr = LogisticRegression(solver='lbfgs', max_iter=10000)
+    
     if use_custom_features:
         # Use TF-IDF together with custom features
         X, y = get_data_with_features(include_stories=use_stories, use_class_grouping=use_class_grouping)
@@ -14,12 +16,11 @@ def logistic_regression_classifier(use_stories=False, use_custom_features=False,
         tfidf_pipe = make_pipeline(ExtractColumn(0), TfidfVectorizer(sublinear_tf=True, use_idf=True))
         custom_pipe = make_pipeline(ExtractColumn(1))
 
-        model = make_pipeline(FeatureUnion([("tfidf", tfidf_pipe), ("custom_features", custom_pipe)]),
-                              LogisticRegression())
+        model = make_pipeline(FeatureUnion([("tfidf", tfidf_pipe), ("custom_features", custom_pipe)]), lr)
     else:
         X, y = get_data(include_stories=use_stories, use_class_grouping=use_class_grouping)
 
-        model = make_pipeline(TfidfVectorizer(sublinear_tf=True, use_idf=True, ngram_range=(1, 2)), LogisticRegression())
+        model = make_pipeline(TfidfVectorizer(sublinear_tf=True, use_idf=True, ngram_range=(1, 2)), lr)
 
     # Evaluate classifier using KFold cross validation
     kf = KFold(n_splits=5, shuffle=True, random_state=77)
